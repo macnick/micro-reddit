58030fdcc6c0125ab5d6aa1a76068528a9083834
macnick <nick@macexperts.gr>
2019-10-31 17:12:11 +0200

    Add in Commenting 3, 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 6 }
     validates :body, presence: true, length: { minimum: 10 }
     belongs_to :user
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     has_many :posts
+    has_many :comments
 end
