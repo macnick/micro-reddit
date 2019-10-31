b67bfc4840327a1865677a926c4d9387c6088c62
macnick <nick@macexperts.gr>
2019-10-31 13:50:52 +0200

    Validations 4, 5, 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :email, presnece: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     validates :password, presence: true
 end
